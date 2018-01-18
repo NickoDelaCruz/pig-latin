ecd97b6af3691650a9338ed3075cc3f81ebd3325
Nicko Dela Cruz and Ron Craig <ron@>
2018-01-18 10:03:05 -0800

    first commit, HTML setup and alert output

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,11 @@
 $(document).ready(function(){
-  $("#info-form").submit(function)(even) {
-    var name = $("input#name").val();
-    var income = $("input#income").val();
-    var education = $("input#education").val();
-    var gender = $("input#gender").val();
-    var birth = $("input#birth").val();
-    var race = $("input#race").val();
-    var information = $("input#info").val();
+  $("form#translate").submit(function(event) {
+    var english = $("input#translator").val();
 
-    $(".name").text(name);
-    $(".income").text(income);
-    $(".education").text(education);
-    $(".gender").text(gender);
-    $(".birth").text(brith);
-    $("race").text(race);
-    $(".information").text(information);
-    $("#survey").fadeToggle();
+
+
+    $("#result").text(english);
+    alert(english);
 
     event.preventDefault();
   });
